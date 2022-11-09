f81149a735007ea455cf9fb45ef4796cf469a78a
Muhammad Adil Raja Saputra <adilraja19@students.undip.ac.id>
2022-11-09 15:30:26 +0700

    Remove command in onBind()

--- app/src/main/java/com/example/musicoletfake/MyService.java
+++ app/src/main/java/com/example/musicoletfake/MyService.java
@@ -11,7 +11,6 @@ public class MyService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         return null;
     }
 
